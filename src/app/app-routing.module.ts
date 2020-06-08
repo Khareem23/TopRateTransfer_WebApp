bd73d8040de74617b57ad4f407c21c07e2bc3fc6
Alagbala Damilola <alagbaladamilola@gmail.com>
2020-06-08 10:26:59 +0100

    Project route setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,38 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeLayoutComponent } from "./_layout/home-layout/home-layout.component";
+import { BodyComponent } from "./home/body/body.component";
+import { AdminLayoutComponent } from "./_layout/admin-layout/admin-layout.component";
+import { DashboardComponent } from "./features/admin/dashboard/dashboard.component";
+import { LoginComponent } from "./auth/login/login.component";
 
+const routes: Routes = [
+  // Home routes goes here
+  {
+    path: "",
+    component: HomeLayoutComponent,
+    children: [{ path: "", component: BodyComponent, pathMatch: "full" }],
+  },
 
-const routes: Routes = [];
+  // Admin routes goes here
+  {
+    path: "admin",
+    component: AdminLayoutComponent,
+    children: [{ path: "dashboard", component: DashboardComponent }],
+  },
+
+  //no layout routes for auth
+  {
+    path: "login",
+    component: LoginComponent,
+  },
+
+  // otherwise redirect home
+  { path: "**", redirectTo: "" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeHeaderComponent } from "./_layout/home-header/home-header.component";
+import { HomeFooterComponent } from "./_layout/home-footer/home-footer.component";
+import { AdminHeaderComponent } from "./_layout/admin-header/admin-header.component";
+import { AdminFooterComponent } from "./_layout/admin-footer/admin-footer.component";
+import { AdminSidebarComponent } from "./_layout/admin-sidebar/admin-sidebar.component";
+import { AdminLayoutComponent } from "./_layout/admin-layout/admin-layout.component";
+import { HomeLayoutComponent } from "./_layout/home-layout/home-layout.component";
+import { BodyComponent } from './home/body/body.component';
+import { DashboardComponent } from './features/admin/dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeHeaderComponent,
+    HomeFooterComponent,
+    HomeLayoutComponent,
+    AdminHeaderComponent,
+    AdminFooterComponent,
+    AdminSidebarComponent,
+    AdminLayoutComponent,
+    BodyComponent,
+    DashboardComponent,
+    LoginComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
